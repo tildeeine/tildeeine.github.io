7954502e8109cbab325d32583505a1a29c66bcf9
tildeeine <t1ld3me@gmail.com>
2023-08-21 15:24:50 +0200

    Removed gulp for now, will reconsider adding back later. Creating project issue for this

--- gulpfile.js
+++
@@ -1,8 +0,0 @@
-import gulp from "gulp";
-import cleanCSS from "gulp-clean-css";
-
-gulp.task("minify-css", function () {
-  return gulp.src("src/css/*.css")
-    .pipe(cleanCSS())
-    .pipe(gulp.dest("dist/css"));
-});
