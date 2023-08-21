12d0a4f7e2d5c9ee2c6726d6a175260313dc61b5
tildeeine <t1ld3me@gmail.com>
2023-08-21 15:07:44 +0200

    Changed syntac of minify-css function, and changed back to require for the imports

--- gulpfile.js
+++ gulpfile.js
@@ -1,7 +1,7 @@
-import gulp from "gulp";
-import cleanCSS from "gulp-clean-css";
+const gulp = require("gulp");
+const cleanCSS = require("gulp-clean-css");
 
-gulp.task("minify-css", () => {
+gulp.task("minify-css", function () {
   return gulp.src("src/css/*.css")
     .pipe(cleanCSS())
     .pipe(gulp.dest("dist/css"));
