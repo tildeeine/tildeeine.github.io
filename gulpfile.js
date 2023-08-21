2b769a4e38fda50dc0c0ca0d6294926667380aba
tildeeine <t1ld3me@gmail.com>
2023-08-21 14:55:22 +0200

    Resolved minor codacy issues in gulpfile

--- gulpfile.js
+++ gulpfile.js
@@ -1,8 +1,8 @@
-const gulp = require('gulp');
-const cleanCSS = require('gulp-clean-css');
+const gulp = require("gulp");
+const cleanCSS = require("gulp-clean-css");
 
-gulp.task('minify-css', () => {
-  return gulp.src('src/css/*.css')
+gulp.task("minify-css", () => {
+  return gulp.src("src/css/*.css")
     .pipe(cleanCSS())
-    .pipe(gulp.dest('dist/css'));
+    .pipe(gulp.dest("dist/css"));
 });
