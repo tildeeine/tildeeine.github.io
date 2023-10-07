c3f3b71a144f86ffc0e86a60dc11a6d97492db4b
tildeeine <t1ld3me@gmail.com>
2023-10-07 01:07:29 +0100

    added first text to about and portfolio sections

--- src/components/AboutSection.tsx
+++ src/components/AboutSection.tsx
@@ -3,6 +3,9 @@ import React from "react";
 const AboutSection: React.FC = () => {
     return (
         <section>
+            <div id='About Me'>
+                <p>About Me</p>
+            </div>
             {/* Your content about you goes here */}
         </section>
     );
--- src/components/PortfolioSection.tsx
+++ src/components/PortfolioSection.tsx
@@ -3,6 +3,9 @@ import React from "react";
 const PortfolioSection: React.FC = () => {
     return (
         <section>
+            <div id='Portfolio'>
+                <p>Portfolio</p>
+            </div>
             {/* Your projects and their descriptions go here */}
         </section>
     );
