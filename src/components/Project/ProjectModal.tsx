bcef12677a75590dcb212894e9d67ca2ec98c71f
tildeeine <t1ld3me@gmail.com>
2024-05-10 17:47:04 +0100

    feat: Started improving modal design, added handler for clicks anywhere on projectcard

--- src/components/Project/ProjectModal.tsx
+++ src/components/Project/ProjectModal.tsx
@@ -19,29 +19,42 @@ const ProjectModal: React.FC<ProjectModalProps> = ({ project, isOpen, onClose })
         return null;
     }
 
+    // Function to handle click on the backdrop
+    const handleBackdropClick = (event: { stopPropagation: () => void; }) => {
+        event.stopPropagation(); // Stop click event from propagating to child elements
+        onClose();
+    };
+
+    // Function to prevent click inside the modal content from closing it
+    const handleModalContentClick = (event: { stopPropagation: () => void; }) => {
+        event.stopPropagation();
+    };
+
     return (
-        <div className="fixed inset-0 bg-black bg-opacity-50 flex justify-center items-center">
-            <div className="bg-white p-5 rounded-lg w-3/4">
-                <button onClick={onClose}>Close</button>
-                <h2>{project.title}</h2>
-                <p>{project.description}</p>
-                <div className="flex">
-                    <div className="w-7/10">
-                        <h3>Languages Used:</h3>
+        <div className="fixed inset-0 bg-black bg-opacity-50 flex justify-center items-center " onClick={handleBackdropClick}>
+            <div className="bg-background p-5 rounded-lg w-3/4 font-poppins" onClick={handleModalContentClick}>
+                <div className="w-7/10 pr-40">
+                    <h2 className="text-darkblue font-bold font-poppins text-xl">{project.title}</h2>
+                    <div className=" text-darkorange mb-4">
                         <ul>{project.languages.map(lang => <li key={lang}>{lang}</li>)}</ul>
-                        <h3>Task:</h3>
+                    </div>
+                    <div className=" text-darkblue mb-4">
+                        <p>{project.description}</p>
+                    </div>
+                    <div className="flex text-darkblue">
+                        <h3>Task</h3>
                         <p>{project.task}</p>
                         <h3>Solution:</h3>
                         <p>{project.solution}</p>
                     </div>
-                    <div className="w-3/10">
-                        {project.images.map((img, index) => (
-                            <img key={index} src={img} alt={`Screenshot ${index + 1}`} className="mb-2" />
-                        ))}
-                    </div>
+                </div>
+                <div className="w-3/10 flex justify-right">
+                    {project.images.map((img, index) => (
+                        <img key={index} src={img} alt={`Screenshot ${index + 1}`} className="mb-2" />
+                    ))}
                 </div>
             </div>
-        </div>
+        </div >
     );
 };
 
