b5b8b6b1d1f1512820c7d4b0f9c2fa1a521e2878
tildeeine <t1ld3me@gmail.com>
2024-05-27 19:01:13 +0100

    fix: Modal in large mode now correct layout, but alignment still a bit off

--- src/components/Project/ProjectModal.tsx
+++ src/components/Project/ProjectModal.tsx
@@ -1,5 +1,5 @@
-import React from 'react';
-import Image from 'next/image';
+import React from "react";
+import Image from "next/image";
 
 interface ProjectModalProps {
     project: {
@@ -8,7 +8,7 @@ interface ProjectModalProps {
         languages: string[];
         task: string;
         solution: string;
-        images: string[];
+        images: string[]; // Array of image URLs
         time: string;
     };
     isOpen: boolean;
@@ -20,22 +20,20 @@ const ProjectModal: React.FC<ProjectModalProps> = ({ project, isOpen, onClose })
         return null;
     }
 
-    const handleBackdropClick = (event: React.MouseEvent) => {
-        event.stopPropagation();
+    // Function to handle click on the backdrop
+    const handleBackdropClick = () => {
         onClose();
     };
 
-    const handleModalContentClick = (event: React.MouseEvent) => {
+    // Function to prevent click inside the modal content from closing it
+    const handleModalContentClick = (event: React.MouseEvent<HTMLDivElement, MouseEvent>) => {
         event.stopPropagation();
     };
 
     return (
         <div className="fixed inset-0 bg-black bg-opacity-50 flex justify-center items-center z-30" onClick={handleBackdropClick}>
-            <div
-                className="modal-container bg-background p-5 rounded-lg w-11/12 md:w-3/4 lg:w-2/3 xl:w-1/2 h-5/6 max-h-screen font-poppins flex flex-col overflow-y-auto"
-                onClick={handleModalContentClick}
-            >
-                <div className="p-4 space-y-4 w-full">
+            <div className="modal-container bg-background p-5 rounded-lg w-11/12 md:w-3/4 lg:w-2/3 xl:w-1/2 h-5/6 max-h-screen overflow-y-auto font-poppins flex flex-col md:flex-row" onClick={handleModalContentClick}>
+                <div className="p-4 space-y-4 w-full md:w-2/3">
                     <div className="flex justify-between items-start">
                         <h2 className="text-3xl font-bold text-darkblue flex-grow">{project.title}</h2>
                         <div className="text-darkblue rounded-full text-md ml-4 flex-shrink-0 mt-1">
@@ -64,18 +62,14 @@ const ProjectModal: React.FC<ProjectModalProps> = ({ project, isOpen, onClose })
                         <h3 className="text-lg font-semibold">Solution</h3>
                         <p>{project.solution}</p>
                     </div>
-                    {/* Project images */}
-                    <div className="flex flex-wrap mt-4">
-                        {project.images.map((image, index) => (
-                            <div key={index} className="w-full sm:w-1/2 md:w-1/3 lg:w-1/4 p-1">
-                                <Image
-                                    src={require(`../../assets/img/${image}`).default}
-                                    alt={project.title}
-                                    className="object-cover w-full h-32 sm:h-48 rounded-lg"
-                                />
-                            </div>
-                        ))}
-                    </div>
+                </div>
+                {/* Project images */}
+                <div className="flex flex-col justify-between md:justify-start lg:justify-between lg:items-end w-full md:w-1/3">
+                    {project.images.map((image, index) => (
+                        <div key={index} className="p-1 flex justify-center md:justify-end lg:justify-start w-full">
+                            <Image src={require(`../../assets/img/${image}`).default} alt={project.title} className="object-cover w-48 h-32 rounded-lg" />
+                        </div>
+                    ))}
                 </div>
             </div>
         </div>
