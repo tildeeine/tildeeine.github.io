bba606336ac378ae37a060745444398ddb2ea482
tildeeine <t1ld3me@gmail.com>
2024-05-27 16:28:50 +0100

    fix: changed textsize

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -7,7 +7,7 @@ interface NavbarProps {
 const Navbar: React.FC<NavbarProps> = ({ isVisible }) => {
     return (
         <nav className={`fixed top-0 left-0 right-0 z-20 bg-background transition-transform duration-300 ${isVisible ? 'translate-y-0' : '-translate-y-full'}`}>
-            <div className="container mx-auto flex justify-between items-center py-4">
+            <div className="container mx-auto flex justify-between items-center py-4 text-lg">
                 <a href={`#home`} className="text-primary hover:underline text-darkorange">Tilde Eriksen Eine</a>
 
                 <ul className="flex text-darkblue">
