65e128bdb58f46f3c690d35c0cf0af93102e38ad
tildeeine <t1ld3me@gmail.com>
2024-05-27 17:11:04 +0100

    fix: Clicking name in navbar now scrolls to top instead of almost to the top

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,14 +1,23 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 
 interface NavbarProps {
     isVisible: boolean;
 }
 
 const Navbar: React.FC<NavbarProps> = ({ isVisible }) => {
+    const scrollToTop = () => {
+        window.scrollTo({
+            top: 0,
+            behavior: "smooth"
+        });
+    };
+
     return (
         <nav className={`fixed top-0 left-0 right-0 z-20 bg-background transition-transform duration-300 ${isVisible ? 'translate-y-0' : '-translate-y-full'}`}>
             <div className="container mx-auto flex justify-between items-center py-4 text-lg">
-                <a href={`#home`} className="text-primary hover:underline text-darkorange">Tilde Eriksen Eine</a>
+                <p className="text-primary hover:underline text-darkorange" onClick={scrollToTop}>
+                    Tilde Eriksen Eine
+                </p>
 
                 <ul className="flex text-darkblue">
                     {['Projects', 'About me', 'Contact'].map((item) => (
