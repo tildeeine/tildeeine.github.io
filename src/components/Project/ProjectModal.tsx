231b35492b5622ee953f0bba49bf65b10302bc08
tildeeine <t1ld3me@gmail.com>
2024-06-02 19:34:51 +0100

    feat: Content updates on projects and about me

--- src/components/Project/ProjectModal.tsx
+++ src/components/Project/ProjectModal.tsx
@@ -50,7 +50,7 @@ const ProjectModal: React.FC<ProjectModalProps> = ({ project, isOpen, onClose })
         <div className={`fixed inset-0 bg-black bg-opacity-50 flex justify-center items-center z-30 ${isOpen ? "block" : "hidden"}`}>
             <div
                 ref={modalRef}
-                className={`bg-background p-5 rounded-lg w-11/12 md:w-3/4 lg:w-2/3 xl:w-1/2 h-5/6 max-h-screen overflow-y-auto font-poppins flex flex-col md:flex-row transform transition-all duration-500 ${isOpen ? "animate-slide-up" : "animate-slide-down"}`}
+                className={`modal-container bg-background p-5 rounded-lg w-11/12 md:w-3/4 lg:w-2/3 xl:w-1/2 h-5/6 max-h-screen overflow-y-auto font-poppins flex flex-col md:flex-row transform transition-all duration-500 ${isOpen ? "animate-slide-up" : "animate-slide-down"}`}
             >
                 <div className="p-4 space-y-4 w-full md:w-2/3">
                     <div className="flex justify-between items-start">
@@ -71,19 +71,23 @@ const ProjectModal: React.FC<ProjectModalProps> = ({ project, isOpen, onClose })
                             <a href={project.link} target="_blank" rel="noopener noreferrer">See project</a>
                         </div>
                     </div>
-                    <div className="text-secondary text-sm">
+                    <div className="text-secondary text-sm mb-4">
                         <h3 className="text-md font-semibold">Task</h3>
-                        <p>{project.task}</p>
+                        <p className="whitespace-pre-line">{project.task}</p>
                     </div>
-                    <div className="text-secondary text-sm">
+                    <div className="text-secondary text-sm mb-4">
                         <h3 className="text-md font-semibold">Solution</h3>
-                        <p>{project.solution}</p>
+                        <p className="whitespace-pre-line pb-4">{project.solution}</p>
                     </div>
                 </div>
                 <div className="flex flex-col justify-between md:justify-start lg:justify-between lg:items-end w-full md:w-1/3 mt-4">
                     {project.images.map((image, index) => (
                         <div key={index} className="p-1 flex flex-wrap justify-center md:justify-end lg:justify-center w-full">
-                            <Image src={require(`../../assets/img/${image}`).default} alt={project.title} className="object-cover h-32 lg:h-36 xl:h-48 rounded-lg" loading="eager"
+                            <Image
+                                src={require(`../../assets/img/${image}`).default}
+                                alt={project.title}
+                                className="object-cover h-32 lg:h-36 xl:h-48 rounded-lg"
+                                loading="eager"
                             />
                             <p className="text-xs text-left text-secondary mb-2 mt-1">
                                 {project.image_description[index]}
