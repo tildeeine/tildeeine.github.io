360b8f7a7ac171515d87e48db324843e2359aa87
tildeeine <t1ld3me@gmail.com>
2024-09-30 19:46:04 +0200

    fix: Alignment fixes for home section

--- src/components/HomeSection.tsx
+++ src/components/HomeSection.tsx
@@ -4,22 +4,23 @@ import profileImage from '../assets/img/frontphoto.webp';
 
 const HomeSection: React.FC = () => {
     return (
-        <section id="home" className="font-poppins mt-5 p-8 mx-auto">
+        <section id="home" className="font-poppins mt-5 md:p-8 mx-auto w-full sm:w-auto">
+
             <div className="flex flex-col lg:flex-row items-center justify-between w-full md:w-4/5 mx-auto">
                 <div className="flex lg:hidden justify-center lg:justify-end mt-8 lg:mt-0 lg:w-1/3">
                     <Image src={profileImage} alt="Tilde Eriksen Eine" className="w-3/4 lg:w-full rounded-full" />
                 </div>
                 <div className="flex-1 lg:w-2/3 text-center lg:text-left">
                     <h1 className="text-5xl lg:text-6xl mt-8 mb-4 text-primary">Tilde Eriksen Eine </h1>
-                    <ul className="text-sm lg:text-base mb-8 ml-2 text-secondary">
+                    <ul className="text-sm lg:text-base mb-8 ml-6 text-secondary list-disc">
                         <li>
-                            5th year student, M.Sc. Cybersecurity and Data Communications @ NTNU Trondheim.
+                            5th year student, <b>M.Sc. Cybersecurity and Data Communications</b> @ NTNU Trondheim.
                         </li>
                         <li>
-                            Member of Ascend NTNU, Control group.
+                            Internships at <b>Mnemonic</b> and <b>Visma</b>.
                         </li>
                         <li>
-                            Internships at Mnemonic and Visma.
+                            Working on drone communication system for <b>Ascend NTNU</b>.
                         </li>
                         <li>
                             Student teaching assistant.
@@ -38,7 +39,7 @@ const HomeSection: React.FC = () => {
                         </button>
                     </div>
                 </div>
-                <div className="hidden lg:flex justify-center lg:justify-end mt-8 lg:mt-0 lg:w-1/3">
+                <div className="hidden lg:flex justify-center lg:justify-end mt-8 lg:mt-0 m-4 lg:w-1/3">
                     <Image src={profileImage} alt="Tilde Eriksen Eine" className="w-3/4 lg:w-full rounded-full" />
                 </div>
             </div>
